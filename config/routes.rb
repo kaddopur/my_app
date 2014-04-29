e0ce7c9812a16243234fc6376d362ade28eaaa49
Kaddo <kaddo.dev@gmail.com>
2014-04-29 12:11:06 +0800

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MyApp::Application.routes.draw do
+  resources :comments
+
   resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429030347) do
+ActiveRecord::Schema.define(version: 20140429040919) do
+
+  create_table "comments", force: true do |t|
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "title"
